270ac04e3f10f263c35a65282fff59590dac67df
Your Name <you@example.com>
2025-09-11 23:36:05 +0200

    add component login

--- app/authentication/login/login.tsx
+++ app/authentication/login/login.tsx
@@ -1,3 +1,14 @@
+import { Button } from "../components/button"
+import { Input } from "../components/input"
+
 export const Login = ()=> {
-    return (<h1>Login</h1>)
+    return (<div className="flex items-center justify-center min-h-screen bg-gray-100">
+          <form className="bg-white p-8 rounded shadow-md w-full max-w-md">
+            <h2 className="text-2xl font-semibold mb-6 text-center">Connexion</h2>
+    
+            <Input label="Email" type="email" /> 
+            <Input label="Mot de passe" type="password" />
+            <Button title="Se connecter"/>
+          </form>
+        </div>)
 }
