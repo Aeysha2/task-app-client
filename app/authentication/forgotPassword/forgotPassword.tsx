37e4b418f78f96ae1f86314f888215e37c0cca55
Your Name <you@example.com>
2025-09-12 22:57:00 +0200

    add forgot password screen

--- app/authentication/forgotPassword/forgotPassword.tsx
+++ app/authentication/forgotPassword/forgotPassword.tsx
@@ -1,3 +1,14 @@
-export const ForgotPassword = ()=> {
-    return (<h1>ForgotPassword</h1>)
-}
+import { Button } from "../components/button";
+import { Form } from "../components/form";
+import { FormTitle } from "../components/formTitle";
+import { Input } from "../components/input";
+
+export const ForgotPassword = () => {
+  return (
+    <Form>
+      <FormTitle title="Mot de passe oublié" />
+      <Input label="Email" type="email" />
+      <Button title="Verifier l'email" />
+    </Form>
+  );
+};
