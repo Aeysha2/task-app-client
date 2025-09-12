47650d6f2169499076c679cf71771e8c7d9b2e02
Your Name <you@example.com>
2025-09-12 22:58:04 +0200

    add reset password screen

--- app/authentication/resetPassword/resetPassword.tsx
+++ app/authentication/resetPassword/resetPassword.tsx
@@ -1,3 +1,13 @@
+import { Button } from "../components/button"
+import { Form } from "../components/form"
+import { FormTitle } from "../components/formTitle"
+import { Input } from "../components/input"
+
 export const ResetPassword = ()=> {
-    return (<h1>ResetPassword</h1>)
+    return (<Form>
+          <FormTitle title="Reinitialisation de mot de passe" />
+          <Input label="Mot de passe" type="password"/>
+          <Input label="Confirmer le mot de passe" type="password" />
+          <Button title="Reinitialiser" />
+        </Form>)
 }
