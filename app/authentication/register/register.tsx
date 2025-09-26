c9cf84e7b65fa58d6eb3ffdf02658ba7db016836
Your Name <you@example.com>
2025-09-26 23:24:15 +0200

    WIP: add user from frontend

--- app/authentication/register/register.tsx
+++ app/authentication/register/register.tsx
@@ -4,25 +4,41 @@ import { Form } from "../components/form";
 import { FormTitle } from "../components/formTitle";
 import { AuthInfo } from "../components/authInfo";
 import { useNavigate } from "react-router";
+import { useState } from "react";
+import { baseUrl } from "~/utils/constante";
 export const Register = () => {
   const navigate = useNavigate()
+  const [firstname, setFirstname] = useState("")
+  const [lastname, setLastname] = useState("")
+  const [email, setEmail] = useState("")
+  const [password, setPasseword] = useState("")
+
   const handlerRegister = (event: React.MouseEvent<HTMLButtonElement>) => {
     event.preventDefault();
-    navigate("/")
-    console.log("handlerRegister");
-  };
+    fetch (`${baseUrl}/users`, {method: "POST",
+      headers : {"Content-type": "application/json"},
+      body:JSON.stringify({firstname,lastname,email,password})})
+    .then(response=> response.json)
+    .then(user =>{
+      console.log(user)
+      //navigate("/") 
+      })
+    .catch((error)=>{
+      console.error("Erreur lors de l'ajout de l'utilisateur:", error) })
+    }
   return (
     <Form>
       <FormTitle title="Inscription" />
-      <Input label="Prénom" type="text" />
-      <Input label="Nom" type="text" />
-      <Input label="Email" type="email" />
-      <Input label="Mot de passe" type="password" />
-      <Input label="Confirmer le mot de passe" type="password" />
+      <Input label="Prénom" type="text" onChange={setFirstname} />
+      <Input label="Nom" type="text" onChange={setLastname}/>
+      <Input label="Email" type="email" onChange={setEmail}/>
+      <Input label="Mot de passe" type="password" onChange={setPasseword} />
+      <Input label="Confirmer le mot de passe" type="password" onChange={setPasseword}/>
       <Button title="S'inscrire" onclick={handlerRegister} />
       
     <AuthInfo action="connectez-vous" answer="Vous avez deja un compte" url="/" />
   
     </Form>
   );
-};
+  };
+
--- app/dashboard/components/addTask.tsx
+++ app/dashboard/components/addTask.tsx
@@ -6,27 +6,30 @@ import { Input } from "~/authentication/components/input";
 import { baseUrl } from "~/utils/constante";
 
 export const AddTask = () => {
-  const [description, setdescription] = useState("")
+  const [description, setDescription] = useState("")
   const [title, setTitle] = useState("")
 
   const addTask = (event:any) => {
     event.preventDefault()
      fetch(`${baseUrl}/tasks`,{method:"POST", 
       headers :{"Content-Type": "application/json"},
-      body:JSON.stringify({description:"Devoir", title:"a faire"})})
+      body:JSON.stringify({description, title})})
       .then(response => response.json())
-      .then(task => console.log(task))
+      .then(task => {
+       
+      })
       .catch((error) => {
     console.error("Erreur lors de la création de la tâche :", error);
+        setDescription("")
+        setTitle("")
   })
-    console.log(description,title)
   };
     
   return (
     <Form position="start">
       <FormTitle title="Nouvelle tâche" />
       <Input label="Titre" type="text" onChange={setTitle} />
-      <Input label="desciption" type="text" onChange={setdescription} />
+      <Input label="desciption" type="text" onChange={setDescription} />
       <Button title="Ajouter" onclick={addTask } />
     </Form>
   );
