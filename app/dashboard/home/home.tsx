a9ffe1e38764780ddac197bb80890d012598bd55
Your Name <you@example.com>
2025-10-03 21:34:12 +0200

    handler userLogged

--- app/dashboard/home/home.tsx
+++ app/dashboard/home/home.tsx
@@ -6,7 +6,7 @@ import type { UserLogged } from "~/types";
 import { getUserFromStorage } from "~/utils/getUserLogged";
 
 export const Home = () => {
-  const [user, setUser] = useState<string|null>(null)
+  const [user, setUser] = useState<UserLogged|null>(null)
   const [tasksVisibility, setTasksVisibility ] = useState(true)
 
   const toggle = () => {
@@ -15,15 +15,14 @@ export const Home = () => {
 
   useEffect(()=>{
     const userLogged:UserLogged|null = getUserFromStorage()
-    setUser(localStorage.getItem(userLoggedKey))
-    console.log("makhoss",userLogged)
-  }, [])
+    setUser(userLogged)
+  }, [user])
   return (
     <>
       <div className="bg-blue-600 p-10 gap-40 flex  justify-center items-center">
         <div className="flex items-center gap-2">
-            <div className="w-10 h-10 bg-white flex  justify-center items-center rounded-full uppercase">YK</div>
-            <small className="text-base uppercase">Yacou keita</small>
+            <div className="w-10 h-10 bg-white flex  justify-center items-center rounded-full uppercase">{user?.firstname[0]}{user?.lastname[0]}</div>
+            <small className="text-base uppercase">{user?.firstname} {user?.lastname}</small>
         </div>
         <h1 className="text-white text-3xl">Application de gestion de tâche</h1>
       </div>
